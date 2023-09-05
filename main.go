ccde5d4d516afb9cf57232484956051a90290204
Joshua Jones <joshua@general-metrics.com>
2023-09-05 13:38:52 -0700

    Add sum file

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func main() {
-	fmt.Println("This is a test")
+	id := uuid.New()
+	fmt.Println("This is a test", id)
 }
